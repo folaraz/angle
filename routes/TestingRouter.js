d54d1dfc1747cde28e87df7b294817c6155cd22b
folaraz <folaraz11@gmail.com>
2022-04-17 22:54:05 +0100

    added more to return

--- routes/TestingRouter.js
+++ routes/TestingRouter.js
@@ -4,9 +4,7 @@ const testingRouter = express.Router();
 
 testingRouter.get("/", async (req, res) => {
     try {
-        const allUserBookings = {
-            id: '1', username: 'olajide abdulrazzaq'
-        }
+        const allUserBookings = [{id: '1', username: 'olajide abdulrazzaq'}, {id: '2', username: 'olajide nasrullah'}]
 
         res.status(200).json(allUserBookings)
     } catch (error) {
