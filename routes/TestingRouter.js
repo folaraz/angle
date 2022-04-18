9f008b772dc0708589d314a0ecd6b6b2d0e2ea2c
folaraz <folaraz11@gmail.com>
2022-04-18 12:47:09 +0100

    change object returned

--- routes/TestingRouter.js
+++ routes/TestingRouter.js
@@ -4,7 +4,13 @@ const testingRouter = express.Router();
 
 testingRouter.get("/", async (req, res) => {
     try {
-        const allUserBookings = [{id: '1', username: 'olajide abdulrazzaq'}, {id: '2', username: 'olajide nasrullah'}]
+        const allUserBookings = [
+            {id: '1', username: 'olajide abdulrazzaq'},
+            {id: '2', username: 'olajide nasrullah'},
+            {id: '3', username: 'olajide basit'},
+            {id: '4', username: 'olajide neemat'},
+
+        ]
 
         res.status(200).json(allUserBookings)
     } catch (error) {
